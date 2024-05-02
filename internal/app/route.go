1dec0a758ccb3b3c881c344d3c48f06a1073852e
arsyad <arsyadrama7@gmail.com>
2024-05-03 02:03:58 +0700

    feat: add feature CRUD group

--- internal/app/route.go
+++ internal/app/route.go
@@ -11,13 +11,26 @@ func setRoute(
 	e *echo.Echo,
 
 	authCtrl controller.AuthCtrl,
+	groupCtrl controller.GroupCtrl,
 ) {
 	e.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, "Hello, World!")
 	})
-	users := e.Group("/v1/users")
+
+	base := e.Group("/v1")
+
+	users := base.Group("/users")
 	{
 		users.POST("/register", authCtrl.UserRegistration)
 		users.POST("/login", authCtrl.UserLogin)
+	}
+
+	groups := base.Group("/groups")
+	{
+		groups.POST("", groupCtrl.CreateGroup)
+		groups.GET("/:id", groupCtrl.GetGroupByID)
+		groups.GET("", groupCtrl.GetGroups)
+		groups.PUT("/:id", groupCtrl.UpdateGroup)
+		groups.DELETE("/:id", groupCtrl.DeleteGroup)
 	}
 }
